e6ffe0380fc1d7458e84abcb2230ef80ba7fbf33
System Administrator <root@Balachandrans-MacBook-Pro.local>
2021-08-08 22:02:46 +0530

    issue fixed

--- src/App.js
+++ src/App.js
@@ -5,26 +5,42 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      imput: "",
+      input: "",
+      isButton: false
     };
   }
   handleChange = (e) => {
-    this.setState({ input: e.target.value });
+    debugger
+    this.setState({ input: e.target.value }, () => {
+      if(this.state.input !== "")
+        this.setState({ isButton: true })
+    });
   };
   handleClear = () => {
     this.setState({ input: "" });
   };
+  handleBlur = (e) => {
+    debugger
+    if(e.relatedTarget !== null && e.relatedTarget.localName === "button")
+    {
+    this.handleClear();
+    this.setState({ isButton: false })
+    }
+    else
+    this.setState({ isButton: false })
+  }
   render() {
     return (
       <div className="App">
         <h4>Click on textbox to see clear btn</h4>
         <input
           type="text"
-          onChange={this.handleChange}
           value={this.state.input}
           className="input-control"
+          onBlur={this.handleBlur}
+          onChange={this.handleChange}
         />
-        <button type="button" onClick={this.handleClear} className="clear">
+        <button type="button" style={{display: this.state.isButton !== false ? "block": "none"}} onClick={this.handleClear} >
           Clear
         </button>
       </div>
